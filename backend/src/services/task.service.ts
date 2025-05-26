98de3a9cd4436b71006a949c8ebf7173375ebc79
Nondumison <nkosindumi77@gmail.com>
2025-05-26 22:11:26 +0200

    refactor: clean up task creation logic and default status

--- backend/src/services/task.service.ts
+++ backend/src/services/task.service.ts
@@ -2,24 +2,6 @@ import { AppDataSource } from "../config/database";
 import { Task } from "../models/Task";
 import { User } from "../models/User";
 
-// export const createTask = async (
-//   userId: number,
-//   taskData: Partial<Task>
-// ): Promise<Task> => {
-//   const taskRepository = AppDataSource.getRepository(Task);
-//   const userRepository = AppDataSource.getRepository(User);
-//   const user = await userRepository.findOneBy({ id: userId });
-//   if (!user) {
-//     throw new Error("User not found");
-//   }
-
-//   const task = taskRepository.create({
-//     ...taskData,
-//     user,
-//   });
-
-//   return await taskRepository.save(task);
-// };
 export const createTask = async (
   userId: number,
   taskData: Partial<Task>
@@ -27,7 +9,6 @@ export const createTask = async (
   const taskRepository = AppDataSource.getRepository(Task);
   const userRepository = AppDataSource.getRepository(User);
   const user = await userRepository.findOneBy({ id: userId });
-
   if (!user) {
     throw new Error("User not found");
   }
@@ -35,7 +16,6 @@ export const createTask = async (
   const task = taskRepository.create({
     ...taskData,
     user,
-    status: taskData.status || "todo", 
   });
 
   return await taskRepository.save(task);
